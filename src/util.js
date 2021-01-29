8455279742d43752747608da88e049ef14c5f1c8
naveen834 <75559553+naveen834@users.noreply.github.com>
2021-01-29 13:30:41 +0530

    Update util.js

--- src/util.js
+++ src/util.js
@@ -2,7 +2,7 @@
 
 export const smallImage = (imagePath, size) => {
   if(imagePath === null){
-    return 'https://i.stack.imgur.com/EMMpP.png';
+    return 'https://media.rawg.io/media/resize/640/-/games/490/49016e06ae2103881ff6373248843069.jpg';
   }
   const image = imagePath.match(/media\/screenshots/)
     ? imagePath.replace(
