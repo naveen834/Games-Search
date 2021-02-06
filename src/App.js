b4f20aba607b07815dbeed76f063c5bbb8863ede
naveen834 <75559553+naveen834@users.noreply.github.com>
2021-02-06 18:15:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 //Components and pages
 //Styles
 import GlobalStyles from './components/GlobalStyles';
 //Router
 import { Route } from 'react-router-dom';
 import Nav from './components/Nav';
+// const Home = React.lazy(() => import('./pages/Home'));
 import Home from './pages/Home';
+import { loadGames } from './actions/gamesAction';
+import { useDispatch } from 'react-redux';
 
 function App() {
   return (
@@ -13,10 +16,19 @@ function App() {
       <GlobalStyles />
       <Route path={['/game/:id', '/']}>
         <Nav />
-        <Home />
+        <HomeDef />
       </Route>
     </div>
   );
+}
+
+function HomeDef() {
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames(setLoading));
+  }, [dispatch]);
+  return <Home loading={loading} />;
 }
 
 export default App;
