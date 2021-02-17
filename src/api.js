5480d2899ce178c5a7dcc99cb785f88fca60727a
naveen834 <75559553+naveen834@users.noreply.github.com>
2021-02-17 19:40:46 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -29,9 +29,9 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 //Popular Games
-const popular_games = `games?key=d9cfd3be14eb49768370f2284682129a&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games = `games?key=d9cfd3be14eb49768370f2284682129a&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const newGames = `games?key=d9cfd3be14eb49768370f2284682129a&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
